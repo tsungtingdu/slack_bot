4c9af1efd91e7b387fd41e63fa91eea4607a10a5
canopus <tsungtingdu@gmail.com>
2018-04-03 22:16:11 +0800

    test auth

--- test.rb
+++ test.rb
@@ -3,3 +3,6 @@ require 'json'
 
 rc = HTTP.post("https://slack.com/api/api.test")
 puts JSON.pretty_generate(JSON.parse(rc.body))
+
+rc = HTTP.post("https://slack.com/api/auth.test", params:{ token: ENV['SLACK_API_TOKEN'] })
+puts JSON.pretty_generate(JSON.parse(rc.body))
