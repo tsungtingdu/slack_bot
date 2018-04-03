a4ff7a0bbdf717f4b2dd4c4c769862f1e33af0e5
canopus <tsungtingdu@gmail.com>
2018-04-03 22:28:17 +0800

    post message on slack channel

--- test.rb
+++ test.rb
@@ -6,3 +6,16 @@ puts JSON.pretty_generate(JSON.parse(rc.body))
 
 rc = HTTP.post("https://slack.com/api/auth.test", params:{ token: ENV['SLACK_API_TOKEN'] })
 puts JSON.pretty_generate(JSON.parse(rc.body))
+
+
+rc = HTTP.post("https://slack.com/api/chat.postMessage", params: { 
+  token: ENV['SLACK_API_TOKEN'], 
+  channel: '#random',
+  text: 'Hello World'
+})
+
+rc = HTTP.post("https://slack.com/api/chat.postMessage", params: {
+  token: ENV['SLACK_API_TOKEN'],
+  channel: '#general',
+  text: 'zzzz'
+})
