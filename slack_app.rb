6034f61598392020860e685b556f5dd80821b7ad
canopus <tsungtingdu@gmail.com>
2018-04-03 23:42:11 +0800

    - create a index.html page with Slack button - create slack_app.rb

---
+++ slack_app.rb
@@ -0,0 +1,60 @@
+require 'http'
+require 'json'
+require 'eventmachine'
+require 'faye/websocket'
+require 'sinatra'
+
+Thread.new do
+  EM.run do
+  end
+end
+
+get '/' do 
+  if params['code']
+    rc = HTTP.post("https://slack.com/api/oauth.access", params: { 
+      client_id: ENV['SLACK_CLIENT_ID'],
+      client_secret: ENV['SLACK_CLIENT_SECRET'],
+      code: params['code'] 
+    })
+
+    token = rc['bot']['bot_access_token']
+
+
+    rc = HTTP.post("https://slack.com/api/rtm.start", params: { 
+      token: token, 
+    })
+    rc = JSON.parse(rc.body)
+
+    url = rc['url']
+
+    EM.run do 
+      ws = Faye::WebSocket::Client.new(url)
+      
+      ws.on :open do
+        p [:open]
+      end
+
+      ws.on :message do |event|
+        data = JSON.parse(event.data)
+        
+        if data['text'] == 'hi'
+          ws.send({
+            type:'message',
+            text: "hi <@#{data['user']}>", 
+            channel: data['channel'] 
+          }.to_json)
+        end  
+        
+        p [:message, JSON.parse(event.data)]  
+      end
+
+      ws.on :close do
+        p [:close, event.code, event.reason]
+        EM.stop
+      end  
+    end
+
+  else  
+    "Hello World"
+  end  
+end
